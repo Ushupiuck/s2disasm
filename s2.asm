f1ebb4f9a840b67adaf5ba20a1fa57786d52b4b7
NyperYuhgard <93950153+NyperYuhgard@users.noreply.github.com>
2023-10-27 04:27:31 -0500

    Fixed ObjC0

--- s2.asm
+++ s2.asm
@@ -72626,9 +72626,9 @@ ObjC0_Init:
 	neg.w	d0
 +
 	move.w	x_pos(a0),d1
-	move.w	d1,objoff_34(a0)
+	move.w	d1,objoff_38(a0)
 	add.w	d1,d0
-	move.w	d0,objoff_32(a0)
+	move.w	d0,objoff_36(a0)
 ; loc_3BF3E:
 ObjC0_Main:
 	moveq	#0,d0
@@ -72653,11 +72653,11 @@ loc_3BF66:
 	beq.s	+++	; rts
 	addq.b	#2,routine_secondary(a0)
 	move.w	#$C00,x_vel(a0)
-	move.w	#$80,objoff_30(a0)
+	move.w	#$80,objoff_34(a0)
 	btst	#0,status(a0)
 	bne.s	+
 	neg.w	x_vel(a0)
-	neg.w	objoff_30(a0)
+	neg.w	objoff_34(a0)
 +
 	jsr	(ObjectMove).l
 	move.b	status(a0),d0
@@ -72670,6 +72670,10 @@ loc_3BF66:
 	andi.b	#p2_standing,d0
 	beq.s	+	; rts
 	lea	(Sidekick).w,a1 ; a1=character
+	bsr.s	loc_3BFB4
++
+	rts
+; ===========================================================================
 
 loc_3BFB4:
 	clr.w	inertia(a1)
@@ -72684,10 +72688,10 @@ loc_3BFB4:
 ; ===========================================================================
 
 loc_3BFD8:
-	move.w	objoff_30(a0),d0
+	move.w	objoff_34(a0),d0
 	add.w	d0,x_vel(a0)
 	jsr	(ObjectMove).l
-	move.w	objoff_32(a0),d0
+	move.w	objoff_36(a0),d0
 	sub.w	x_pos(a0),d0
 	btst	#0,status(a0)
 	beq.s	+
@@ -72697,7 +72701,7 @@ loc_3BFD8:
 	bpl.s	loc_3C034
 	move.b	status(a0),d0
 	andi.b	#standing_mask,d0
-	beq.w	++
+	beq.s	return_3C01E
 	move.b	d0,d1
 	andi.b	#p1_standing,d1
 	beq.s	+
@@ -72705,21 +72709,24 @@ loc_3BFD8:
 	bsr.s	loc_3BFB4
 +
 	andi.b	#p2_standing,d0
-	beq.w	++
+	beq.s	return_3C01E
 	lea	(Sidekick).w,a1 ; a1=character
-	bra.s	loc_3BFB4
+	bsr.s	loc_3BFB4
+
+return_3C01E:
+	rts
 ; ===========================================================================
 
 loc_3C020:
 	move.w	x_vel(a0),x_vel(a1)
 	move.w	#-$400,y_vel(a1)
 	bset	#1,status(a1)
-+	rts
+	rts
 ; ===========================================================================
 
 loc_3C034:
 	addq.b	#2,routine_secondary(a0)
-	move.w	objoff_32(a0),x_pos(a0)
+	move.w	objoff_36(a0),x_pos(a0)
 	move.b	status(a0),d0
 	andi.b	#standing_mask,d0
 	beq.s	loc_3C062
@@ -72738,20 +72745,20 @@ loc_3C056:
 loc_3C062:
 	move.w	x_pos(a0),d0
 	moveq	#4,d1
-	tst.w	objoff_30(a0)	; if objoff_30(a0) is positive,
+	tst.w	objoff_34(a0)	; if objoff_34(a0) is positive,
 	spl	d2		; then set d2 to $FF, else set d2 to $00
 	bmi.s	+
 	neg.w	d1
 +
 	add.w	d1,d0
-	cmp.w	objoff_34(a0),d0
+	cmp.w	objoff_38(a0),d0
 	bhs.s	+
 	not.b	d2
 +
 	tst.b	d2
 	bne.s	+
 	clr.b	routine_secondary(a0)
-	move.w	objoff_34(a0),d0
+	move.w	objoff_38(a0),d0
 +
 	move.w	d0,x_pos(a0)
 	rts
